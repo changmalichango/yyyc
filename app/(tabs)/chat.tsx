64a3496523292ebc2f4eb1e3b8e49a2153f06c5b
yy43yy <yiyang15624@gmail.com>
2025-05-27 17:19:41 +0800

    Update tab icons for Chat and Me screens

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: "#0066cc", fontSize: 20 }}>
-        Welcome to my Home screen 👋
+      <Text style={{ color: "#0066cc", fontSize: 30 }}>
+        This is the chat fuction 👋
       </Text>
     </View>
   );
---
+++ app/(tabs)/me.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "#0066cc", fontSize: 30 }}>
+        Welcome to my Home screen 👋
+      </Text>
+    </View>
+  );
+}
