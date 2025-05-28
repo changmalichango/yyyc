0abdad813b7eaa54a24bcf355b7270cda51658f3
yy43yy <yiyang15624@gmail.com>
2025-05-28 23:53:41 +0800

    login and register put in one page and after login successfully, user will be redirect to home page

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "#0066cc", fontSize: 30 }}>
+        This is the home fuction 👋
+      </Text>
+    </View>
+  );
+}
