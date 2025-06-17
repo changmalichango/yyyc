fb99b76aa312b6acae9ee02472eb7ff956764dc9
yy43yy <yiyang15624@gmail.com>
2025-06-17 13:57:04 +0800

    list ui mostly done

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import { Ionicons } from "@expo/vector-icons";
 import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
   useColorScheme,
 } from "react-native";
@@ -17,7 +19,15 @@ export default function ListScreen() {
   const textColor = colorScheme === "dark" ? styles.lightText : styles.darkText;
   return (
     <SafeAreaView style={[styles.container, styleColor]}>
+      {/* TITLE
+      ////////////////////////////////////////////////////////////////////////////
+      */}
+
       <Text style={[styles.title, textColor]}>List your item</Text>
+
+      {/* ITEM NAME BOX
+        //////////////////////////////////////////////////////////////////////// */}
+
       <ScrollView style={styles.container2}>
         <View>
           <Text style={[textColor, styles.textOnBox]}>Name</Text>
@@ -29,20 +39,53 @@ export default function ListScreen() {
           </View>
         </View>
 
+        {/* CONDITON BOX
+        ///////////////////////////////////////////////////////////////////////// */}
+        <View>
+          <Text style={[textColor, styles.textOnBox]}>Conditions</Text>
+
+          <View style={[styles.box2]}>
+            <TextInput placeholder="$" />
+          </View>
+          <View style={{ justifyContent: "center", marginRight: 43 }}>
+            {/* <Text
+              style={[
+                textColor,
+                { fontWeight: "bold", fontSize: 15, paddingLeft: 10 },
+              ]}
+            >
+              $
+            </Text> */}
+          </View>
+        </View>
+
+        {/* PRICE BOX 
+        //////////////////////////////////////////////////////////////////////// */}
+
         <View>
           <Text style={[textColor, styles.textOnBox]}>Price</Text>
           <View style={{ flexDirection: "row" }}>
             <View style={[styles.box2]}>
               <TextInput placeholder="$" />
             </View>
-            <View style={{ justifyContent: "center", marginRight: 55 }}>
-              <Text>$</Text>
+            <View style={{ justifyContent: "center", marginRight: 43 }}>
+              <Text
+                style={[
+                  textColor,
+                  { fontWeight: "bold", fontSize: 15, paddingLeft: 10 },
+                ]}
+              >
+                $
+              </Text>
             </View>
             <View style={[styles.box2]}>
-              <Text>per month</Text>
+              <Text style={textColor}>per month</Text>
             </View>
           </View>
         </View>
+
+        {/* DESCRIPTION NAME BOX
+        //////////////////////////////////////////////////////////////////////// */}
 
         <View>
           <Text style={[textColor, styles.textOnBox]}>Description</Text>
@@ -53,19 +96,47 @@ export default function ListScreen() {
                 paddingLeft: 5,
                 fontSize: 15,
                 paddingTop: 5,
-                // justifyContent: "flex-end",
               }}
             />
           </View>
         </View>
 
+        {/* IMAGE BOX
+        ////////////////////////////////////////////////////////////////////////// */}
         <View>
-          <Text style={[textColor, styles.textOnBox]}>Image</Text>
-          <View style={[styles.box1]}></View>
-
-          <View style={[styles.box1]}>
-            <Text>Upload Image</Text>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={[textColor, styles.textOnBox]}>Image</Text>
+            <Text style={{ marginTop: 15, color: "grey" }}>
+              (maximum of 3 images)
+            </Text>
           </View>
+
+          {/* <View style={[styles.imageBox]}></View> */}
+          <TouchableOpacity style={styles.imageBox}>
+            <Ionicons name="add" size={40} color={"grey"} />
+          </TouchableOpacity>
+          {/* 
+          <TouchableOpacity style={styles.box1}>
+            <Text>Upload Image</Text>
+          </TouchableOpacity> */}
+        </View>
+
+        {/* ADDRESS
+        //////////////////////////////////////////////////////////////// */}
+        <View>
+          <Text style={[textColor, styles.textOnBox]}>Address</Text>
+          <View style={[styles.box1]}>
+            <TextInput
+              placeholder="Enter your address"
+              style={{ paddingLeft: 5, fontSize: 15 }}
+            />
+          </View>
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.uploadBox}>
+            <Text style={{ fontWeight: "bold", color: "white" }}>UPLOAD</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -86,14 +157,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: 20,
   },
-  textOnBox: { fontWeight: "bold", marginTop: 10 },
+  textOnBox: { fontWeight: "bold", marginTop: 15, marginBottom: 2 },
   box1: {
     // backgroundColor: "blue",
     height: 50,
     width: "95%",
     borderWidth: 1,
     borderRadius: 4,
-    borderColor: "grey",
+    borderColor: "#bbbbbb",
     justifyContent: "center",
   },
   box2: {
@@ -112,5 +183,28 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderColor: "grey",
     // justifyContent: "center",
+  },
+  imageBox: {
+    color: "grey",
+    height: 50,
+
+    width: 50,
+    borderWidth: 2,
+    borderRadius: 4,
+    borderColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
+    borderStyle: "dashed",
+  },
+  uploadBox: {
+    marginTop: 15,
+    backgroundColor: "darkgreen",
+    height: 50,
+    width: "95%",
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "grey",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
