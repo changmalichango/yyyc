7b9c06dda711789afc8b81172605dbe194a9a8e4
Jung Yoochan <jungyoochan@Jungs-MacBook-Air-2.local>
2025-06-18 00:06:31 +0800

    base done, abstraction tmr

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -9,7 +9,7 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-  useColorScheme,
+  useColorScheme
 } from "react-native";
 
 export default function ListingsScreen() {
@@ -34,48 +34,103 @@ export default function ListingsScreen() {
       image: require("../../assets/images/shoe.png"),
     },
   ];
+  const renderIteming = ({item}: { item: any}) => (
+    <View style={styles.itemCard}>
+      <Image source={item.image} style= {styles.image} />,
+      <Text> style={styles.itemTitle}{item.title}</Text>
+      <Text> style={[styles.price, textTheme]}${item.price}</Text>
+      <Text style={[styles.username, textTheme]}>@{item.username}</Text>
+    </View>
+  )
 
   return (
     <SafeAreaView style={[styles.safe, themeStyle]}>
       {/* ///////////////////////////////////// */}
       {/* TOP BAR AND SEARCHING BAR!!!!!!!!!!!! */}
-      <View style={styles.title}>
-        <Image
-          source={require("../../assets/images/logo.png")}
-          style={styles.logo}
-        />
-        <Text style={styles.logoText}>CanIRent</Text>
-      </View>
+      <View style ={styles.topSection}>
+        <View style={styles.title}>
+          <Image
+            source={require("../../assets/images/logo.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.logoText}>CanIRent</Text>
+        </View>
 
-      {/* ///////////////////////////////////// */}
-      {/* THIS IS THE SEARCHING BAR!!!!!!!!!!!! */}
-      <View style={styles.search}>
-        <FontAwesome name="search" size={24} style={styles.searchIcon} />
-        <TextInput placeholder="Search" style={{ width: "75%" }} />
-        <TouchableOpacity style={styles.searchBtn}>
-          <Text style={styles.btnText}>Go!</Text>
-        </TouchableOpacity>
+        {/* ///////////////////////////////////// */}
+        {/* THIS IS THE SEARCHING BAR!!!!!!!!!!!! */}
+        <View style={styles.search}>
+          <FontAwesome name="search" size={24} style={styles.searchIcon} />
+          <TextInput placeholder="Search" style={{ width: "75%" }} />
+          <TouchableOpacity style={styles.searchBtn}>
+            <Text style={styles.btnText}>Go!</Text>
+          </TouchableOpacity>
+        </View>
       </View>
+      
+     <ScrollView contentContainerStyle = {styles.scrollview}>
+      <View style={styles.bottomSection}>
+        <View>  
+          <Image
+          source = {require("../../assets/images/bike.png")} 
+          style={styles.bike}
+          /> 
+        </View>
 
-      <ScrollView contentContainerStyle={styles.container}>
-        {items.map((item) => (
-          <View key={item.id} style={styles.card}>
-            <Image
-              source={item.image}
-              style={styles.image}
-              resizeMode="contain"
-            />
-            <Text style={[styles.label, textTheme]}>{item.title}</Text>
-          </View>
-        ))}
+        <View>  
+          <Image
+          source = {require("../../assets/images/guitar.png")} 
+          style={styles.guitar}
+          /> 
+        </View>
+      </View>
       </ScrollView>
-    </SafeAreaView>
+
+</SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   safe: {
     flex: 1,
+  },
+  scrollview: {
+    padding: 20,
+  },
+  topSection: {
+    padding: 20,
+  },
+  bottomSection: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  leftHalf: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+  rightHalf: {
+    flex: 1,
+  },
+  listContainer: {
+    padding: 10,
+  },
+  itemCard:{
+    alignItems: 'center',
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+    paddingTop: 2,
+  },
+  price: {
+    fontSize:16,
+    color: '#28a745',
+    fontWeight: 600,
+  },
+  username:{
+    fontSize: 12,
+    color: '#888',
   },
   lightColor: { backgroundColor: "#fff" },
   darkColor: { backgroundColor: "black" },
@@ -128,4 +183,50 @@ const styles = StyleSheet.create({
   card: { marginBottom: 24, alignItems: "center" },
   image: { width: 150, height: 150 },
   label: { marginTop: 8, fontSize: 18, fontWeight: "600" },
+  bike: {
+    resizeMode: "contain",
+    height: 150,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 50
+  },
+  guitar: {
+    resizeMode: "contain",
+    height: 150,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 40,
+  },
 });
+
+
+/*      <ScrollView contentContainerStyle = {styles.scrollview}>
+      <View style={styles.bottomSection}>
+        <View>  
+          <Image
+          source = {require("../../assets/images/bike.png")} 
+          style={styles.bike}
+          /> 
+        </View>
+
+        <View>  
+          <Image
+          source = {require("../../assets/images/guitar.png")} 
+          style={styles.guitar}
+          /> 
+        </View>
+      </View>
+      </ScrollView>
+    </SafeAreaView>
+
+    <ScrollView>
+        <View style={styles.bottomSection}>
+          <Image source={ require('../../assets/images/bike.png')} style={styles.image} />
+          <Text style={styles.itemTitle}> Bike </Text>
+          <Text style={styles.price}> $20 </Text>
+          <Text style={styles.username}> @yy134</Text>    
+        </View>
+      </ScrollView>
+*/
